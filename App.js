665d4c32237bc793280f07e56fe715bcddb37373
Cecily <cecily.downs@gmail.com>
2018-04-04 19:19:53 -0700

    Add CameraScreen and SettingsScreen to StackNavigator in App.js

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { StackNavigator } from 'react-navigation';
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import CameraScreen from './screens/CameraScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 const App = StackNavigator({
-  Home: { screen: HomeScreen }
+  Home: { screen: HomeScreen },
+  Camera: { screen: CameraScreen },
+  Settings: { screen: SettingsScreen }
 })
 
 
