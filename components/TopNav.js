32bd4cd50c8030c7a7f3b53e0945f54eb52a2bcb
Cecily <cecily.downs@gmail.com>
2018-04-04 19:20:36 -0700

    Work on header formatting, add icons, and add experimental button

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
 export default class TopNav extends Component {
   render() {
+
+    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.topNav}>
-        <Text style={styles.dateText}>Thursday, 14th March</Text>
-        <Text style={styles.welcome}>Welcome Rey!</Text>
+        <View style={styles.left}>
+          <Image 
+            style={{ width: 30, height: 25 }}
+            source={require("../assets/icons/camera.png")} />
+        </View>
+        <View style={styles.middle}>
+          <Text style={styles.dateText}>Thursday, 14th March</Text>
+          <Text style={styles.welcome}>Welcome Rey!</Text>
+        </View>
+        <View style={styles.right}>
+          <Image 
+              style={{ width: 30, height: 30 }}
+              source={require("../assets/icons/settings.png")} />
+
+           <Button title="Click" onPress={() => navigate('Camera')} />
+        </View>
       </View>
     );
   }
@@ -13,11 +31,17 @@ export default class TopNav extends Component {
 
 const styles = StyleSheet.create({
   topNav: {
-    flexDirection: "column",
-    justifyContent: "flex-start",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "white",
     paddingTop: 30
+  },
+  left: {
+    padding: 10
+  },
+  right: {
+    padding: 10
   },
   welcome: {
     fontSize: 20,
