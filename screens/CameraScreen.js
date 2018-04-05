f0011bd249153174b3abfb8ec227aec954616d98
Cecily <cecily.downs@gmail.com>
2018-04-04 19:20:49 -0700

    Add CameraScreen

---
+++ screens/CameraScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button, Alert } from "react-native";
+
+import TopNav from "../components/TopNav";
+
+type Props = {};
+export default class CameraScreen extends Component<Props> {
+  constructor(props) {
+    super(props);
+    //example binding function
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Camera page</Text>
+      </View>
+    );
+  }
+}
+
+HomeScreen.navigationOptions = {
+    header: (
+        <TopNav />
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
