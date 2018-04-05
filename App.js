232c9ba675c9ae9b48ca4cddaff6e697f5a5f39b
Cecily <cecily.downs@gmail.com>
2018-04-04 18:58:00 -0700

    Remove home screen content from App.js and add react-navigation StackNavigator

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button, Alert } from "react-native";
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen'
 
-import Weather from "./components/Weather";
-import Feed from "./components/Feed";
-import TopNav from "./components/TopNav";
+const App = StackNavigator({
+  Home: { screen: HomeScreen }
+})
 
-type Props = {};
-export default class App extends Component<Props> {
-  constructor(props) {
-    super(props);
-    //example binding function
-  }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TopNav />
-        <Weather />
-        <Feed />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
+export default App
