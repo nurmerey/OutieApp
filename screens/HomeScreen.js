5715f534d01e2fb025a1b3cf003458cae9919010
Cecily <cecily.downs@gmail.com>
2018-04-04 18:57:20 -0700

    Add home page content to HomeScreen with custom header option

---
+++ screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button, Alert } from "react-native";
+
+import Weather from "../components/Weather";
+import Feed from "../components/Feed";
+import TopNav from "../components/TopNav";
+
+type Props = {};
+export default class HomeScreen extends Component<Props> {
+  constructor(props) {
+    super(props);
+    //example binding function
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Weather />
+        <Feed />
+      </View>
+    );
+  }
+}
+
+HomeScreen.navigationOptions = {
+    header: (
+        <TopNav />
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
