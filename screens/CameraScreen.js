a47317cb88c9e45011141410d961cb8b6011eeb7
Cecily <cecily.downs@gmail.com>
2018-04-10 21:23:32 -0700

    Implementing react-native-camera

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button, Alert } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  TouchableOpacity
+} from "react-native";
+
+import { RNCamera } from "react-native-camera";
 
 import TopNav from "../components/TopNav";
 
@@ -13,14 +23,59 @@ export default class CameraScreen extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Camera page</Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={
+            "We need your permission to use your camera phone"
+          }
+        />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.warn("MY PHOTO", data.uri);
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
   }
 });
